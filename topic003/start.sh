cc203013f8c12a51aec21178af9a0fbcb452936d
colynn.liu <colynn.liu@gmail.com>
2021-04-22 21:52:23 +0800

    jenkins image export port chang to 8090

--- topic003/start.sh
+++ topic003/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d -p 8080:8080 -p 50000:50000 -v ~/Testenv/jenkins_home:/var/jenkins_home --name jenkins jenkins/jenkins:2.277.1-lts-alpine
+docker run -d -p 8090:8080 -p 50000:50000 -v ~/Testenv/jenkins_home:/var/jenkins_home --name jenkins jenkins/jenkins:2.277.1-lts-alpine
